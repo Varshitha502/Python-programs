58e334e0ed500847ddc4825e5893b47ac7914f0c
Chella Varshitha <varshitha1436@gmail.com>
2026-04-24 18:55:33 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class math():
+    def add(self,a,b):
+        return a+b
+    def mul(self,a,b):
+        return a*b
+obj1=math()
+ans1=obj1.add(2,8)
+print(ans1)
+obj2=math()
+ans2=obj2.mul(2,7)
+print(ans2)
